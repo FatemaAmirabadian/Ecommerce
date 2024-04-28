93ce70bb3ebd11f46ac58662c05ac6af91889b09
Fateme <fff.amirabadian@gmail.com>
2024-04-29 00:22:55 +0330

    resolve deploy errors

--- src/app/admin/productmanagement/newProduct/page.js
+++ src/app/admin/productmanagement/newProduct/page.js
@@ -1,6 +1,6 @@
 import NewProduct from "@/app/components/templates/NewProduct";
 
-function page() {
+function Page() {
   return (
     <div>
       <NewProduct />
@@ -8,4 +8,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
