10c4183f5c77885826b54fda5ca480195b388ad3
Fateme <fff.amirabadian@gmail.com>
2024-04-29 14:53:40 +0330

    user cards style

--- src/app/components/modules/UserCard.js
+++ src/app/components/modules/UserCard.js
@@ -1,15 +1,34 @@
-"use client";
 import React from "react";
 
 function UserCard({ user }) {
-  console.log({user});
+  console.log({ user });
   return (
-    <div className="product_card flex">
-        <div>{user.email}</div>
-        <div>{user.password}</div>
-        <div>{user.firstName}</div>
-        <div>{user.lastName}</div>
+    <div className="bg-white rounded-lg shadow-lg p-4 md:p-6 lg:p-8">
+      <div className="mb-4">
+        <label className="block text-sm font-bold mb-2" htmlFor="email">
+          Email
+        </label>
+        <div className="text-lg">{user.email}</div>
       </div>
+      <div className="mb-4">
+        <label className="block text-sm font-bold mb-2" htmlFor="password">
+          Password
+        </label>
+        <div className="text-lg">{user.password}</div>
+      </div>
+      <div className="mb-4">
+        <label className="block text-sm font-bold mb-2" htmlFor="firstName">
+          First Name
+        </label>
+        <div className="text-lg">{user.firstName}</div>
+      </div>
+      <div className="mb-4">
+        <label className="block text-sm font-bold mb-2" htmlFor="lastName">
+          Last Name
+        </label>
+        <div className="text-lg">{user.lastName}</div>
+      </div>
+    </div>
   );
 }
 
