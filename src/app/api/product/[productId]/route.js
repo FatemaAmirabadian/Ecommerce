9148f392992db95b1a1c709b7af411f8bcd8f0b2
Fateme <fff.amirabadian@gmail.com>
2024-04-29 13:40:09 +0330

    productDetails api

--- src/app/api/product/[productId]/route.js
+++ src/app/api/product/[productId]/route.js
@@ -2,32 +2,50 @@ import Product from "@/models/Product";
 import connectDB from "@/utils/connectDB";
 import { NextResponse } from "next/server";
 
+export async function OPTIONS() {
+  // Set CORS headers for OPTIONS requests
+  return NextResponse.json(
+    {},
+    {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin":
+          "https://e-commerce-eight-sand.vercel.app", // Allow requests only from this domain
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE", // Specify allowed methods
+        "Access-Control-Allow-Headers": "Content-Type, Authorization", // Specify allowed headers
+      },
+    }
+  );
+}
+
+
 export async function GET(request, context) {
   try {
     await connectDB();
-  } catch (err) {
-    console.log(err);
-    return NextResponse.json({ error: "error in connect db" }, { status: 500 });
-  }
 
-  const id = context.params.productId;
-  const data = await Product.findOne({ id });
-  try {
+    // Extract the product ID from the request parameters
+    const { productId } = context.params;
+
+    // Find the product in the database using the product ID
+    const product = await Product.findOne({ id: productId });
+
+    
+    if (!product) {
+      return NextResponse.json({ error: "Product not found" }, { status: 404 });
+    }
+
+    
     return NextResponse.json(
-      { data },
-      { 
+      { data: product },
+      {
         status: 200,
         headers: {
-          "Access-Control-Allow-Origin": "https://e-commerce-eight-sand.vercel.app/", // Allow requests only from this domain
-          // "Access-Control-Allow-Methods": "GET", // Allow only GET requests
+          "Access-Control-Allow-Origin": "https://e-commerce-eight-sand.vercel.app",
         },
       }
     );
-  } catch (err) {
-    console.log(err);
-    return NextResponse.json({
-      status: "failed",
-      message: "Error in storing data in DB",
-    });
+  } catch (error) {
+    console.error("Error fetching product details:", error);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
   }
 }
